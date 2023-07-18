ed99a392fbfee4ef7086bc61bc3e89ae4a07833d
Averus89 <alanzur@gmail.com>
2023-07-19 01:03:39 +0200

    fixes to footer

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Route, Switch, Redirect } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
 
-        <Route path="/home">
-          <HomePage />
-        </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
 
-        <Route path="/search">
-          <SearchBooksPage />
-        </Route>
-      </Switch>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
   );
